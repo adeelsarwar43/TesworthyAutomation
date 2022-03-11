213f173427cf7d5c27cc2597020730a5163c2959
adeel <adeel.sarwar43@gmail.com>
2022-03-11 21:37:06 +0500

    new

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -13,8 +13,17 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+require("cypress-xpath");
+
 // Import commands.js using ES2015 syntax:
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// Remove extra logs 
+Cypress.Server.defaults({
+    whitelist: (xhr) => {
+        return true;
+    }
+})
