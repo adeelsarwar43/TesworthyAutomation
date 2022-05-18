76d88f974ea6e40d0b24b294629c08762bea6907
adeel <adeel.sarwar43@gmail.com>
2022-05-18 11:39:12 +0500

    Error free

--- cypress/integration/Testworthy/main.js
+++ cypress/integration/Testworthy/main.js
@@ -14,7 +14,7 @@ describe('Testworthy Smoke Testing', function()
        })
        Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from
-        // failing the test
+        
         return false
       })
 it('Login',function() {
